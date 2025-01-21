e0d0c89983b0f30f18769e6b74b65527bba78042
Ashish Dasari <59196150+ashishdasari148@users.noreply.github.com>
2025-01-21 14:26:19 +0530

    Ts error resolve

--- xrextras/src/aframe/components/lifecycle-components.ts
+++ xrextras/src/aframe/components/lifecycle-components.ts
@@ -4,6 +4,8 @@ declare const XRExtras: any
 declare const XR8: any
 
 interface AFrameElement extends HTMLElement {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   object3D: THREE.Object3D
 }
 
